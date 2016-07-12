3600cdec43ef7b1b972982cb0ceef63c89cf347f
mdcarmo <marc29dias@yahoo.com.br>
2016-07-12 15:44:14 -0300

    inserção código fonte

---
+++ Demo1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo1.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		public JsonResult GetName()
+		{
+			return Json(new { name = "World from server side" }, JsonRequestBehavior.AllowGet);
+		}
+
+	}
+}
